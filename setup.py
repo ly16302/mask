72715ac10e4e9b8cd14217b0d4b8898e9d261be9
Liang Yang <liang.yang@cranfield.ac.uk>
2021-07-24 23:20:08 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
    author="Liang Yang",
    author_email="liang.yang@cranfield.ac.uk",
    description="Example package to demonstrate wheel issue",
-   packages=['mask', 'mask.extension_package'],
-   ext_modules=[Extension('mask', sources=['src/mask_interpolation.f90'])],
+   packages=['example', 'example.extension_package'],
+   ext_modules=[Extension('example', sources=['src/mask_interpolation.f90'])],
 )
