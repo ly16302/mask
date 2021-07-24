e9ca69d65f2ba66d91e38e1c07d022b589a3a612
Liang Yang <liang.yang@cranfield.ac.uk>
2021-07-24 23:45:52 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup, find_packages, Extension
+#!/usr/bin/env python3
+import setuptools
+import site
 import sys 
+from numpy.distutils.core import setup, Extension
+
+site.ENABLE_USER_SITE = True
 
 setup(
-   name="example",
+   name="mask",
    version="0.0.1",
    author="Liang Yang",
    author_email="liang.yang@cranfield.ac.uk",
    description="Example package to demonstrate wheel issue",
-   packages=['example', 'example.extension_package'],
-   ext_modules=[Extension('example', sources=['src/mask_interpolation.f90'])],
+
+   ext_modules=[Extension('mask_interpolation', sources=['src/mask_interpolation.f90'])],
 )
