51e52f6b84de985e1d092f3662b3fdaa9f5ad6e6
Liang Yang <liang.yang@cranfield.ac.uk>
2021-07-25 13:59:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,5 @@ site.ENABLE_USER_SITE = True
 setup(
    name="mask",
    version="0.0.1",
-   author="Liang Yang",
-   author_email="liang.yang@cranfield.ac.uk",
-   description="Example package to demonstrate wheel issue",
-   long_description = "file: README.md",
    ext_modules=[Extension('pre_processing', sources=['src/mask_interpolation.f90'])],
 )
