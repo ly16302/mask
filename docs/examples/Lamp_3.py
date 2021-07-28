4e78a606d4ece08df93b2ed10a864b3484eb8a35
Liang Yang <liang.yang@cranfield.ac.uk>
2021-07-29 00:09:13 +0100

    Update Lamp_3.py

--- docs/examples/Lamp_3.py
+++ docs/examples/Lamp_3.py
@@ -3,11 +3,13 @@ This example showcases:
 - using a points to generate masks
 """
 
+import stltovoxel
 from pre_processing import mask
 import numpy as np
-import scipy.io
 from pyevtk.hl import gridToVTK
-point=np.loadtxt("output.xyz")
+
+stltovoxel.convert_file('3-lamp.stl','resolution',100,'output.npy') #generate a voxel numpy file 
+point=np.load("output.npy")
 
 n=5
 nxd=15*n
@@ -20,6 +22,7 @@ nzp=nzd+1
 xp=np.linspace(0,150,nxp)
 yp=np.linspace(0,180,nyp)
 zp=np.linspace(0,1000,nzp)
+
 mask1=mask.mask_interpolation_non_uniform (point,xp,yp,zp)
 gridToVTK("mask1",xp,yp,zp,cellData = {"mask1":mask1})
 np.save('result',mask1)
